27fa5a6b03895261fb51c6535fa2db4e9d05f256
leowang <rfv7855659@gmail.com>
2023-02-23 15:45:35 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -26,36 +26,37 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
       events: 'rollup-plugin-node-polyfills/polyfills/events',
       stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-      _stream_duplex:'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+      _stream_duplex: 'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
       _stream_passthrough: 'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
       _stream_readable: 'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
       _stream_writable: 'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
-      _stream_transform:'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+      _stream_transform: 'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+      process: 'rollup-plugin-node-polyfills/polyfills/process-es6'
     }
   },
   optimizeDeps: {
     esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis',
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                process: true,
-                buffer: true
-            }),
-            NodeModulesPolyfillPlugin()
-        ]
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
     }
   },
   build: {
-      rollupOptions: {
-          plugins: [
-              // Enable rollup polyfills plugin
-              // used during production bundling
-              rollupNodePolyFill()
-          ]
-      }
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill()
+      ]
+    }
   }
 })
